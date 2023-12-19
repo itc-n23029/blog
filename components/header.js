54f2f9b5bd00949e34a48157564083a55d221398
itc-n23029 <n23029@std.it-college.ac.jp>
2023-12-19 07:50:19 +0000

    4.1 util.module.css

--- components/header.js
+++ components/header.js
@@ -1,11 +1,14 @@
 import Logo from 'components/logo'
 import Nav from 'components/nav'
+import styles from 'styles/header.module.css'
 
 const Header = () => {
   return (
     <header>
-      <Logo box0n />
-      <Nav />
+      <div className={styles.flexContainer}>
+       <Logo box0n />
+       <Nav />
+      </div>
     </header>
   )
 }
