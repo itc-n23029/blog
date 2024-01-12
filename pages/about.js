ab571739ba45a8ffa0fbfcf2dd921d4227a7d857
itc-n23029 <n23029@std.it-college.ac.jp>
2024-01-12 03:30:56 +0000

    chapter8 finished

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,6 @@
-import Meta from 'components/meta'
 import PostBody from 'components/post-body'
-import Container from 'components/container'
 import Hero from 'components/hero'
+import Container from 'components/container'
 import Contact from 'components/contact'
 import {
   TwoColumn,
@@ -10,6 +9,7 @@ import {
 } from 'components/two-column'
 import Image from 'next/image'
 import eyecatch from 'images/about.jpg'
+import Meta from 'components/meta'
 
 const About = () => {
   return (
@@ -19,41 +19,38 @@ const About = () => {
         pageDesc='About development activities'
         pageImg={eyecatch.src}
         pageImgW={eyecatch.width}
-        pageOmgH={eyecatch.height}
+        pageImgH={eyecatch.height}
       />
-      <Hero title='About' subtitle='Aboutdevelopment activities' />
-      <figure>
-        <Image
-          src={eyecatch}
-          alt=''
-          layout='responsive'
-          sizes='(min-width: 1152px) 1152px, 100vw'
-        />
-      </figure>
+      <Hero title='About' subtitle='About development activities' />
+      <Image
+        src={eyecatch}
+        alt=''
+        sizes='(min-width:1152px) 1152px, 100vw'
+        priority
+        placeholder='blur'
+        style={{
+          width: '100%',
+          height: 'auto',
+          transition: '0.2s'
+        }}
+      />
+
       <TwoColumn>
         <TwoColumnMain>
           <PostBody>
             <p>
-              Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインな
-              ど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案
-              し続けています。
+              Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
             </p>
             <h2>モノづくりで目指していること</h2>
             <p>
-              モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいこと
-              を取り入れながら、ユーザーにマッチした提案を実現するのが目標です。たくさんの開発・提供
-              が数多くありますが、特にそこを磨く作業に力を入れています。
+              モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいことを取り入れながら、ユーザーにマッチした提案を実現するのが目標です。たくさんの開発・提供が数多くありますが、特にそこを磨く作業に力を入れています。
             </p>
             <p>
-              単純に形にするだけでなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくり
-              をしています。毎回課題解決テーマをもって「モノ」と向き合い制作をし、フィードバックして
-              もらうことで自分の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
+              単純に形にするだけでなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしています。毎回課題解決テーマをもって「モノ」と向き合い制作をし、フィードバックしてもらうことで自分の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
             </p>
-            <h3> 新しいことへのチャレンジ</h3>
+            <h3>新しいことへのチャレンジ</h3>
             <p>
-              今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでも
-              いいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み
-              出すようなモノづくりは、これからも続けていきたいです。
+              今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
             </p>
           </PostBody>
         </TwoColumnMain>
@@ -64,4 +61,5 @@ const About = () => {
     </Container>
   )
 }
+
 export default About
