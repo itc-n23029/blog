7d72eac58fe24468cab751bd351446c7c133bbcc
itc-n23029 <n23029@std.it-college.ac.jp>
2024-01-16 07:14:17 +0000

    chapter8_add

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,10 +1,60 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar
+} from 'components/two-column'
+import Image from 'next/image'
+import PostCategories from 'components/post-categories'
+import ConvertBody from 'components/convert-body'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
 
-const Schedule = ({ title, publish, content, eyecatch, categories }) => {
+const Schedule = ({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+  description
+}) => {
   return (
     <Container>
-      <h1>記事のタイトル</h1>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <article>
+        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=''
+            layout='responsive'
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes='(min-width: 1152px) 1152px, 100vw'
+            priority
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
     </Container>
   )
 }
@@ -14,13 +64,15 @@ export async function getStaticProps () {
   const slug = 'schedule'
 
   const post = await getPostBySlug(slug)
+  const description = extractText(post.content)
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
       eyecatch: post.eyecatch,
-      categories: post.categories
+      categories: post.categories,
+      description: description
     }
   }
 }
